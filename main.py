eafc8b8a65275d2928ada8eed30f8cd9936190af
MKazmierczak02 <mkazmierczak024@gmail.com>
2021-12-21 17:09:18 +0100

    Find the new overworldbug // new max level

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import pygame, sys
 from settings import *
 from overworld import Overworld
+from level import Level
+
 pygame.init()
 
-class Game():
+
+class Game:
     def __init__(self):
-        self.max_level = 3
-        self.overworld = Overworld(1,self.max_level,screen)
-    
+        self.max_level = 2
+        self.overworld = Overworld(0, self.max_level, screen, self.create_level)
+        self.status = "overworld"
+
+    def create_level(self, current_level):
+        self.level = Level(current_level, screen, self.create_overworld)
+        self.status = "level"
+
+    def create_overworld(self, current_level, new_max_level):
+        if new_max_level > self.max_level:
+            self.max_level = new_max_level
+        self.orverworld = Overworld(
+            current_level, self.max_level, screen, self.create_level
+        )
+        self.status = "overworld"
+
     def run(self):
-        self.overworld.run()
-        
+        if self.status == "overworld":
+            self.overworld.run()
+        else:
+            self.level.run()
 
 
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-game=Game()
+game = Game()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit
             sys.exit()
-    
-    screen.fill('black')
+
+    screen.fill("black")
     game.run()
     pygame.display.update()
     clock.tick(60)
